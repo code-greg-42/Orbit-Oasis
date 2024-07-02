cba73db93e1d816100727692cf1fcd7ebac07c89
greg <grandersson1991@gmail.com>
2024-07-02 15:01:12 -0400

    added animal movement
    set up basic script for navmeshagent movement of animals

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -1,10 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Animal : Item
 {
     [SerializeField] private float foodPerUnit;
+    [SerializeField] private float roamRadius;
 
     public float FoodPerUnit => foodPerUnit;
+
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        SetRandomDestination();
+    }
+
+    private void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            SetRandomDestination();
+        }
+    }
+
+    private void SetRandomDestination()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * roamRadius;
+        randomDirection += transform.position;
+
+        if (NavMesh.SamplePosition(randomDirection, out NavMeshHit hit, roamRadius, NavMesh.AllAreas))
+        {
+            agent.SetDestination(hit.position);
+        }
+    }
 }
