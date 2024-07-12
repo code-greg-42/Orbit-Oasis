d1cb76b9a19b4fd637757b185d391bea5c71413a
greg <grandersson1991@gmail.com>
2024-07-12 14:53:57 -0400

    added player movement to fishing scene

---
+++ Assets/Scripts/Fishing/FishingPlayerMovement.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FishingPlayerMovement : MonoBehaviour
+{
+    private float moveSpeed = 50.0f;
+    private float boostForce = 30.0f;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform playerObject;
+
+    // keybinds
+    private KeyCode boostKey = KeyCode.LeftShift;
+    private KeyCode boostKeyAlt = KeyCode.Space;
+
+    private Vector2 movement;
+    private bool isFlipped = false;
+    private bool setBoost = false;
+
+    private void Update()
+    {
+        GetInput();
+        FlipSprite();
+        RotateVisual();
+
+        if (Input.GetKeyDown(boostKey) || Input.GetKeyDown(boostKeyAlt))
+        {
+            setBoost = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+
+        if (setBoost)
+        {
+            Boost();
+            setBoost = false;
+        }
+    }
+
+    private void GetInput()
+    {
+        // get inputs from WASD keys
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        // add force to move the player
+        rb.AddForce(movement * moveSpeed);
+    }
+
+    private void Boost()
+    {
+        // Apply an impulse force in the direction of movement
+        rb.AddForce(movement.normalized * boostForce, ForceMode2D.Impulse);
+    }
+
+    private void FlipSprite()
+    {
+        if (movement.x < 0 && !isFlipped)
+        {
+            FlipToFacingLeft();
+        }
+        else if (movement.x > 0 && isFlipped)
+        {
+            FlipToFacingRight();
+        }
+    }
+
+    private void FlipToFacingLeft()
+    {
+        Vector3 scale = playerObject.localScale;
+        scale.x = -Mathf.Abs(scale.x);
+        playerObject.localScale = scale;
+        isFlipped = true;
+    }
+
+    private void FlipToFacingRight()
+    {
+        Vector3 scale = playerObject.localScale;
+        scale.x = Mathf.Abs(scale.x);
+        playerObject.localScale = scale;
+        isFlipped = false;
+    }
+
+    private void RotateVisual()
+    {
+        Vector2 velocity = rb.velocity;
+
+        if (velocity != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+
+            if (isFlipped)
+            {
+                angle += 180;
+            }
+
+            playerObject.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+}
