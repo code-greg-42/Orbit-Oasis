db8f0ddd864572f5525e807bde0af1d1709c1140
greg <grandersson1991@gmail.com>
2024-07-17 15:16:29 -0400

    fixed bug with animals getting stuck
    added a stuck timer which sets a new destination after a given time if an animal should happen to get stuck

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -8,6 +8,12 @@ public class Animal : Item
     [SerializeField] private float foodPerUnit;
     [SerializeField] private float roamRadius;
 
+    // anti stuck variables
+    private float stuckThresholdSqr = 0.01f;
+    private float stuckTime = 30.0f; // keeping this high because it's cute when they get stuck for a bit, just not forever.
+    private float stuckTimer = 0.0f;
+    private Vector3 lastPosition;
+
     public float FoodPerUnit => foodPerUnit;
 
     private NavMeshAgent agent;
@@ -34,7 +40,25 @@ public class Animal : Item
             if (!agent.pathPending && agent.remainingDistance < 0.5f)
             {
                 SetRandomDestination();
+                stuckTimer = 0.0f; // reset timer when setting new destination
             }
+
+            // check if stuck
+            if ((transform.position - lastPosition).sqrMagnitude < stuckThresholdSqr)
+            {
+                stuckTimer += 0.5f;
+                if (stuckTimer >= stuckTime)
+                {
+                    SetRandomDestination(); // force new destination
+                    stuckTimer = 0.0f; // reset timer
+                }
+            }
+            else
+            {
+                stuckTimer = 0.0f; // reset timer if moved
+            }
+
+            lastPosition = transform.position; // update recent position
             yield return new WaitForSeconds(0.5f);
         }
     }
