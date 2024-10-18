a4e27f0ec65cca4f1d8dbb6becb5ca51f097bf26
greg <grandersson1991@gmail.com>
2024-10-18 15:21:54 -0400

    added a load saved animals method to animal manager
    saved animals are now properly loaded into the scene at the beginning of the scene and their references are added to the tracking list.

--- Assets/Scripts/AnimalManager.cs
+++ Assets/Scripts/AnimalManager.cs
@@ -14,12 +14,21 @@ public class AnimalManager : MonoBehaviour
         ActiveAnimals = new List<Animal>();
     }
 
-    public void AddActiveAnimal(Animal animal)
+    private void Start()
+    {
+        LoadSavedAnimals();
+    }
+
+    public void AddActiveAnimal(Animal animal, bool saveToData = true)
     {
         ActiveAnimals.Add(animal);
         Debug.Log("Animal added to active animals list.");
         Debug.Log("Active Animal Count: " + ActiveAnimals.Count);
-        UpdateDataManager();
+
+        if (saveToData)
+        {
+            UpdateDataManager();
+        }
     }
 
     public void RemoveActiveAnimal(Animal animal)
@@ -34,6 +43,34 @@ public class AnimalManager : MonoBehaviour
         else
         {
             Debug.LogWarning("Attempted to remove an animal that was not in the active animals list.");
+        }
+    }
+
+    private void LoadSavedAnimals()
+    {
+        if (DataManager.Instance.SavedActiveAnimals.ItemList.Count > 0)
+        {
+            // use copy of list in case of any changes
+            List<AnimalData> savedAnimals = DataManager.Instance.SavedActiveAnimals.ItemList;
+
+            // get item prefab array
+            GameObject[] itemPrefabs = InventoryManager.Instance.ItemPrefabs;
+            
+            foreach (AnimalData animalData in savedAnimals)
+            {
+                // instantiate new prefab at saved position and rotation
+                GameObject animalObject = Instantiate(itemPrefabs[animalData.prefabIndex], animalData.position, animalData.rotation);
+
+                // add new prefab to active animals list
+                if (animalObject.TryGetComponent(out Animal animal))
+                {
+                    ActiveAnimals.Add(animal);
+                }
+                else
+                {
+                    Debug.LogWarning("Incorrect item prefab index likely as instantiated object did not have an Animal component.");
+                }
+            }
         }
     }
 
