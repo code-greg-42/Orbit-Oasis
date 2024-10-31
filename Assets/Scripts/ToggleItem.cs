da8ae53567db4a3b6d181fa97e93cc7ab43ab3f6
greg <grandersson1991@gmail.com>
2024-10-31 16:06:38 -0400

    restructured toggle item logic
    new restructuring better allows for easy creation of future toggle items. any future toggle item will only need to add the specific logic it wishes to toggle into the 'on toggle' method upon inheriting from toggle item.

--- Assets/Scripts/ToggleItem.cs
+++ Assets/Scripts/ToggleItem.cs
@@ -11,5 +11,39 @@ public abstract class ToggleItem : Item
 
     public override bool IsDroppable { get; } = false;
 
-    public abstract void ToggleAbility();
+    public void ToggleAbility()
+    {
+        if (Time.time - recentToggleTime > toggleCooldown)
+        {
+            isActive = !isActive;
+
+            string alertPrefix;
+            MainSoundManager.SoundEffect soundEffect;
+
+            if (isActive)
+            {
+                alertPrefix = "enabling";
+                soundEffect = MainSoundManager.SoundEffect.HacksOn;
+            }
+            else
+            {
+                alertPrefix = "disabling";
+                soundEffect = MainSoundManager.SoundEffect.HacksOff;
+            }
+
+            // show alert message
+            string alertMessage = $"{alertPrefix} {ItemName.ToLower()}...";
+            MainUIManager.Instance.ShowAlertText(alertMessage, 2.5f);
+
+            // play sound effect
+            MainSoundManager.Instance.PlaySoundEffect(soundEffect);
+
+            recentToggleTime = Time.time;
+
+            // execute specific toggle logic
+            OnToggle();
+        }
+    }
+
+    protected abstract void OnToggle();
 }
--- Assets/Scripts/ToggleItems/GravityHacks.cs
+++ Assets/Scripts/ToggleItems/GravityHacks.cs
@@ -4,37 +4,9 @@ using UnityEngine;
 
 public class GravityHacks : ToggleItem
 {
-    public override void ToggleAbility()
+    protected override void OnToggle()
     {
-        if (Time.time - recentToggleTime > toggleCooldown)
-        {
-            isActive = !isActive;
-
-            string alertPrefix;
-            MainSoundManager.SoundEffect soundEffect;
-
-            if (isActive)
-            {
-                alertPrefix = "enabling";
-                soundEffect = MainSoundManager.SoundEffect.HacksOn;
-            }
-            else
-            {
-                alertPrefix = "disabling";
-                soundEffect = MainSoundManager.SoundEffect.HacksOff;
-            }
-
-            // show alert message
-            string alertMessage = alertPrefix + " gravity hacks...";
-            MainUIManager.Instance.ShowAlertText(alertMessage, 2.5f);
-
-            // play sound effect
-            MainSoundManager.Instance.PlaySoundEffect(soundEffect);
-
-            // call gravity hacks method from main game manager
-            MainGameManager.Instance.ToggleGravityHacks();
-
-            recentToggleTime = Time.time;
-        }
+        // call gravity hacks method from main game manager
+        MainGameManager.Instance.ToggleGravityHacks();
     }
 }
